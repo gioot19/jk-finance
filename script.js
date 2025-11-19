86293e44fed9e110463c418c4c702e02f6681edd
gioot19 <giovannatimoteo2@gmail.com>
2025-11-18 21:41:26 -0300

    save

---
+++ script.js
@@ -0,0 +1,51 @@
+//seleciona o botão do menu (icone do hamburguer) pelo id definido no HTML
+const menuBtn = document.getElementById('menu-btn')
+
+//seleciona o elemento de navegação (menu que sera aberto/fechado)
+const navMenu
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 1 criar o menu hamburguer (div e icon) no html e no menu mobile
+// 2 fora do @midia, inserirum display none na div que guarda o icon do hamburguer e na lista do menu mobile
+// 3 dentro do @media inserir o display none no menu desktop
+// 4 dentro do @media aplicar o display flex na div do menu mobile que estava oculta, com isso ela ficara visivel
+// 5 realizar a estilização do nav do menu mobile no css,alinhamento, cores e transição 
+// 6 fazer o javascript conversar com o icon do menu hamburguer e realizar a ação 
